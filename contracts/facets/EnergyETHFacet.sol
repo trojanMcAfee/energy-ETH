9ec4dcc47555f4b66847916b87ab6f08f12814da
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2023-03-21 09:51:49 +0100

    Before modifying helpers

--- contracts/facets/EnergyETHFacet.sol
+++ contracts/facets/EnergyETHFacet.sol
@@ -1,17 +1,20 @@
-// // SPDX-License-Identifier: GPL-2.0-or-later
-// pragma solidity 0.8.19;
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.19;
 
 
-// import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-// import './ozOracleFacet.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/utils/Address.sol';
+import './ozOracleFacet.sol';
 
 
-// contract EnergyETHFacet {
+contract EnergyETHFacet {
 
 
-//     function getEnergyPrice() external returns(uint256) {
+    function getEnergyPrice() external returns(uint256) {
+        bytes memory data = abi.encodeWithSignature('getLastPrice');
+        data = Address.functionCall(address(this), data);
+        return abi.decode(data, (uint256));
+    }
 
-//     }
 
-
-// }
+}
