d93e6250e5dc540c879161d710dd69092d94e600
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2023-04-08 15:09:34 +0200

    Before installing UC module

--- test/foundry/EnergyETH.t.sol
+++ test/foundry/EnergyETH.t.sol
@@ -38,10 +38,6 @@ contract EnergyETHTest is Test {
         "PermitBatch(PermitDetails[] details,address spender,uint256 sigDeadline)PermitDetails(address token,uint160 amount,uint48 expiration,uint48 nonce)"
     );
 
-    bytes32 public constant _PERMIT_BATCH_TRANSFER_FROM_TYPEHASH = keccak256(
-        "PermitBatchTransferFrom(TokenPermissions[] permitted,address spender,uint256 nonce,uint256 deadline)TokenPermissions(address token,uint256 amount)"
-    );
-
     bytes32 public constant _PERMIT_TRANSFER_FROM_TYPEHASH = keccak256(
         "PermitTransferFrom(TokenPermissions permitted,address spender,uint256 nonce,uint256 deadline)TokenPermissions(address token,uint256 amount)"
     );
