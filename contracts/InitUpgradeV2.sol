2252c8c7b614395a0be45658bceba9faf4c3aa91
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2024-03-06 23:09:06 +0100

    Done with InitUpgradeV2

--- contracts/InitUpgradeV2.sol
+++ contracts/InitUpgradeV2.sol
@@ -5,14 +5,23 @@ pragma solidity 0.8.19;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import './AppStorage.sol';
 import '../libraries/LibDiamond.sol';
-// import 'hardhat/console.sol';
 
-import "forge-std/console.sol";
 
+/**
+ * @title Initiates version 2 of OZL system.
+ * @notice Sets the new storage variables needed for eETH to run
+ */
 contract InitUpgradeV2 {
 
     AppStorage s;
 
+    /**
+     * @dev Only function that initiates new storage within the diamond. 
+     * @param feeds_ chainlink price feeds + volatility index. 
+     * @param nonRevFacets_ facets/contracts that don't require a check for
+     * admin revenue when calling the ozDiamond.
+     * @param otherVars_ miscellaneous storage vars. 
+     */
     function init(
         address[] calldata feeds_,
         address[] memory nonRevFacets_,
