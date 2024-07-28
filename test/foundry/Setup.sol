6ee14a8a3e4b2d5708ffe0affb3feb3d60f3862b
dnyrm <cdgmachado0@gmail.com>
2024-07-29 00:34:14 +0200

    Old _getGarchParams() works

--- test/foundry/Setup.sol
+++ test/foundry/Setup.sol
@@ -57,15 +57,6 @@ contract Setup is Test {
     address internal wtiChailink = 0x594b919AD828e693B935705c3F816221729E7AE8;
     address internal goldChainlink = 0x1F954Dc24a49708C26E0C1777f16750B5C6d5a2c;
 
-    // struct Garch {
-    //     uint alpha;
-    //     uint beta;
-    //     uint lastConditional;
-    //     uint lastResidual;
-    //     uint mu;
-    //     uint omega;
-    // }
-
 
     IERC20 USDT = IERC20(0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9);
 
@@ -129,7 +120,7 @@ contract Setup is Test {
 
     function _getGarchParams() private view returns(Garch memory) {
         string memory root = vm.projectRoot();
-        string memory path = string.concat(root, '/garch/params/exponential.json');
+        string memory path = string.concat(root, '/garch/params/eth/exponential.json');
         string memory json = vm.readFile(path);
         bytes memory garchData = vm.parseJson(json);
         return abi.decode(garchData, (Garch));
